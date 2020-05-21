e75635ea2e2bf1d3b9154f64ef974cde4e250be5
guistetter <guilherme_stetter@hotmail.com>
2020-05-21 18:48:41 -0300

    Add Yup para validacao criacao de usuario, senha, email... validando a session

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,9 +1,24 @@
 import User from "../models/User";
-
+import * as Yup from "yup";
 class UserController {
   //criar usuario
   async store(req, res) {
-    const userExists = await User.findOne({ where: { email: req.body.email } });
+    //inicio validacao yup
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "validation fails" });
+    }
+    //fim validacao yup
+
+    const userExists = await User.findOne({
+      where: { email: req.body.email },
+    });
+
     if (userExists) {
       return res.status(400).json({ error: "User already exists" });
     }
@@ -16,7 +31,28 @@ class UserController {
       provider,
     });
   } //Fim criar usuario
+
   async update(req, res) {
+    //inicio validacao yup
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when("oldPassword", (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when("password", (password, field) =>
+        password ? field.required().oneOf([Yup.ref("password")]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "validation fails" });
+    }
+    //fim validacao yup
+
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
