7401b2b083fd29463530600038ee4a922134f6c5
guistetter <guilherme_stetter@hotmail.com>
2020-05-29 13:52:45 -0300

    tratamento de excecoes com sentry, criando rota para errors,  instalando Youch para tratar as msgs de erro

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 import express from "express";
 import path from "path";
+import Youch from "youch";
+import * as Sentry from "@sentry/node";
+import "express-async-errors"; //Obrigatorio ver antes das rotas
 import routes from "./routes";
+import sentryConfig from "./config/sentry";
 import "./database"; //conexao com o bd
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler()); //obrigatoriamente vem antes das rotas !
     this.server.use(express.json());
     this.server.use(
       "/files",
@@ -17,6 +24,14 @@ class App {
   }
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler()); //obrigatoriamente vem depois das rotas!
+  }
+  //cadastrar novo middleware para tratamento de excecao
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 export default new App().server;
---
+++ src/config/sentry.js
@@ -0,0 +1,5 @@
+export default {
+  dsn:
+    "https://1a983528f3334a45b195bcbba174b393@o400080.ingest.sentry.io/5258133",
+};
+//dsn obtida qdo configuramos o projeto no site sentry.io
